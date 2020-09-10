60043ddbc810a54e64877819f61c4edee9c1ae09
onefreewalk <da.eddy@gmailcom>
2020-09-09 17:40:27 -0700

    finished array methods in dom and wealth calc

---
+++ DOMArrayMethods/sandbox.js
@@ -0,0 +1,37 @@
+// array.prototype.sort()
+
+// The sort() method sorts the elements of an array in place and returns the stored array.
+// The default sort order is ascending, built upon converting the elements into strings, then 
+// comparing their sequences of UTF-16 code units values.
+
+    // const arr = [1, 2, 110, 3, 4, 330];
+
+    // const sortedArray = arr.sort(function(a, b) {
+    //   return a - b;
+    // });
+
+    // console.log("This is the sort output:");
+    // console.log(sortedArray);
+
+// array.prototype.filter()
+// filter() method created a new array with all elements that pass the test implemented by the provided function.
+
+    // const arr = [20, 23, 25, 30, 21, 50, 60]
+
+    // const under30 = arr.filter(function(item) {
+    //   return item < 30;
+    // });
+
+    // console.log("This is the filter output:");
+
+    // console.log(under30);
+
+// array.prototype.reduce()
+// reduce() method executes a reducer function(that you provide) on each element to the array.
+// returning a single output value.
+
+  const arr = [1,2,3,4,5,6,7];
+
+  const total = arr.reduce((acc, nextNum) => (acc + nextNum), 0);
+
+  console.log(total);
--- DOMArrayMethods/script.js
+++ DOMArrayMethods/script.js
@@ -1,55 +1,112 @@
-const main = document.getElementById('main');
-const addUserBtn = document.getElementById('add-user');
-const doubleBtn = document.getElementById('double');
-const showMillinairesBtn = document.getElementById('show-millionaires');
-const sortBtn = document.getElementById('sort');
-const calculateWealthBtn = document.getElementById('calculate-wealth');
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillinairesBtn = document.getElementById("show-millionaires");
+const show100kBtn = document.getElementById("show-100k");
+const sortBtn = document.getElementById("sort");
+const sortPoorBtn = document.getElementById("sort-poor");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
 
 let data = [];
 
-
 // fetch random user and add money
-  async function getRandomUser() {
-  const res = await fetch('https://randomuser.me/api');
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
 
   const user = data.results[0];
 
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
-    money: Math.floor(Math.random() * 1000000)
+    money: Math.floor(Math.random() * 1000000),
   };
   // Add a new user as part of getRandomUser
   addData(newUser);
 }
 
-  // Add new obj to data arr
-  function addData(obj) {
-    data.push(obj);
+// Double the Wealth field
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
 
-    updateDOM();
-  }
+  updateDOM();
+}
 
-  //Update DOM
-  function updateDOM(providedData = data) {
-    // Clear main div
-    main.innerHTML = '<h2><strong>Person </strong> Wealth</h2>';
-    providedData.forEach(item => {
-      const element = document.createElement('div');
-      element.classList.add('person');
-      element.innerHTML = `<strong>${item.name} </strong> ${formatMoney(
-        item.money
-      )}`;
-      main.appendChild(element);
-    });
-  }
+// Sort the users by wealth
+// Richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
 
-  // Format number as money
-  // https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
-  //
-  function formatMoney(number) {
-    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-  }
+  updateDOM();
+}
+// Sort the users by wealth
+// Poorest
+function sortByPoorest() {
+  data.sort((a, b) => a.money - b.money);
 
-  // Event Listeners
-  addUserBtn.addEventListener('click', getRandomUser);
+  updateDOM();
+}
+
+// Filter by Millionaires
+function showMillionaires() {
+  data = data.filter((user) => user.money > 1000000);
+
+  updateDOM();
+}
+
+// calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    console.log(wealth);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3> Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+
+  main.appendChild(wealthEl);
+}
+
+// Only show those at 100k and more with
+function showHundredThou() {
+  data = data.filter((user) => user.money > 100000);
+
+  updateDOM();
+}
+
+// Add new obj to data
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+//Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = "<h2><strong>Person </strong> Wealth</h2>";
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name} </strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money
+// https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+//
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+// Event Listeners
+addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
+sortPoorBtn.addEventListener("click", sortByPoorest);
+showMillinairesBtn.addEventListener("click", showMillionaires);
+show100kBtn.addEventListener("click", showHundredThou);
+calculateWealthBtn.addEventListener("click", calculateWealth);
