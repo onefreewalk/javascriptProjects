a2fa9abecdcb10ef0324394716d7ca5885fc5d80
onefreewalk <da.eddy@gmailcom>
2020-09-11 10:46:14 -0700

    finished landing page

---
+++ menuSliderAndModal/app.js
@@ -0,0 +1,20 @@
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
+
+// Navigation Toggle
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
+
+// Display Modal
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+
+// Hide Modal
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
+
+// Hide modal from outer click
+window.addEventListener("click", (e) =>
+  e.target == modal ? modal.classList.remove("show-modal") : false
+);
